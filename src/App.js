e1b339a32c136d4ec4b216bb14c18db023d11546
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-24 23:24:55 -0400

    testing loading component and created test api call to server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import './App.scss';
+import { useState } from 'react';
 import {HeroSection} from '../src/components/HeroSection/HeroSection';
 import {SearchForm} from '../src/components/SearchForm/SearchForm';
+import { Loading } from './components/Loading/Loading';
+import axios from 'axios';
 
 function App() {
+  
+  const [response, setResponse] = useState('')
+
+  const searchChannel = (channel) => {
+    axios
+      .get(`http://localhost:4444/channel/${channel}`)
+      .then((res) => {
+        setResponse(res.data.channel)
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <div className="app">
       <HeroSection/>
-      <SearchForm/>
+      <SearchForm handler = {searchChannel} />
+      {response ?
+        <p style = {{color:"white", textAlign:"center"}}> {response} </p>
+        : <Loading/>
+        }
     </div>
   );
 }
