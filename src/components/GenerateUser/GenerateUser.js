cb55374074b128f883b8e42fcb5e68867b8f9d8e
Cristopher Cunas <cristophercunas@CristopComputer.lan>
2022-05-26 11:07:08 -0400

    added bg color and text header

--- src/components/GenerateUser/GenerateUser.js
+++ src/components/GenerateUser/GenerateUser.js
@@ -7,15 +7,17 @@ export const GenerateUser = ({channel}) => {
   
   return (
     <div className="user">
-      <Paper className="user__container" elevation={2}>
+      <Paper className="user__container" elevation={3} style = {{backgroundColor:"hsla(0,0%,100%,.05)"}}>
         {err ? 
           <p className= "user__error"> User not found </p>
           :
           <Grid container spacing={1} className="user__grid">
             <Grid item xs={6}>
+              <p className="user__grid_header"> Channel </p> <br/>
               <p className="user__grid_name"> {display_name} </p>
             </Grid>
             <Grid item xs={6}>
+              <p className="user__grid_header"> Followers</p> <br/>
               <p className="user__grid_followers"> {followers} </p>
             </Grid>
           </Grid>
